cadb35c17d7a6e6783130fe9cba424fde8ce026a
Iryna B <imbigdash@gmail.com>
2023-05-21 16:37:00 +0100

    refactored code using useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
@@ -6,61 +6,56 @@ import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function Feedback(){
+const [good, setGood] = useState(0);
+const [neutral, setNeutral] = useState(0);
+const [bad, setBad] = useState(0);
+
+  const updateStatistice = option => {
+    if (option === 'good') {
+      setGood( prevState => prevState + 1);
+    } else if (option === 'neutral') {
+      setNeutral(prevState => prevState + 1);
+    } else if (option === 'bad') {
+      setBad(prevState => prevState + 1);
+    }
   };
 
-  updateStatistice = option => {
-    return this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
+  const countTotalFeedback = () => {
+        return good + neutral + bad;
+      };
+    
+   const countPositiveFeedbackPercentage = () => {
+        const positiveRewev = good + neutral;
+        const totalRewev = good + neutral + bad;
+        const persenOfPositiveRewev = Math.floor(
+          (positiveRewev / totalRewev) * 100
+        );
+        return totalRewev !== 0 ? persenOfPositiveRewev : '0';
+      }
 
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const good = this.state.good;
-    const neutral = this.state.neutral;
-    const bad = this.state.bad;
-
-    const positiveRewev = good + neutral;
-    const totalRewev = good + neutral + bad;
-    const persenOfPositiveRewev = Math.floor(
-      (positiveRewev / totalRewev) * 100
-    );
-
-    return totalRewev !== 0 ? persenOfPositiveRewev : '0';
-  };
-
-  render() {
-    return (
-      <>
-        <Section title="Please leave feedback" children={FeedbackOptions}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.updateStatistice}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          {this.state.good + this.state.neutral + this.state.bad > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </>
-    );
-  }
+return (
+        <>
+          <Section title="Please leave feedback" children={FeedbackOptions}>
+            <FeedbackOptions
+              options={['good', 'neutral', 'bad']}
+              onLeaveFeedback={updateStatistice}
+            />
+          </Section>
+  
+          <Section title="Statistics">
+            {good + neutral + bad > 0 ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={countTotalFeedback()}
+                positivePercentage={countPositiveFeedbackPercentage()}
+              ></Statistics>
+            ) : (
+              <Notification message="There is no feedback" />
+            )}
+          </Section>
+        </>
+      );
 }
